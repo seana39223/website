f6b8462c16ee22f16b1e65e364a114e4414bfe9a
Sean Anderson <seana39223@gmail.com>
2019-07-19 15:07:33 +0100

    Feat: Updated text on homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>Hi! I'm Sean</h1>
     <p>
-      I'm a software developer at <a href="https://www.designmynight.com/" target="_blank">DesignMyNight</a>
+      I'm a software developer at <a href="https://www.designmynight.com/" target="_blank">DesignMyNight</a> </br> I built this website from scratch using Vue as a framework and Firebase for deployment. 
     </p>
     <p>
       Just a heads up, this website is still under construction.
