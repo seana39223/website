40e8466ec18b54a41a961e2e1d5b0eff23d0c0aa
Sean Anderson <seana39223@gmail.com>
2019-07-21 20:34:17 +0100

    refactor: Removed duplicate code helloworld renamed to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="hello">
-    <h1>Hi! I'm Sean</h1>
-    <p>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/cutmypic.png')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">Hi! I'm Sean</h1>
+        <p>
       I'm a software developer at <a href="https://www.designmynight.com/" target="_blank">DesignMyNight</a> </br> I built this website from scratch using Vue as a framework and Firebase for deployment. 
-    </p>
+        </p>
     <p>
       Just a heads up, this website is still under construction.
     </p>
-  </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
-
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class Home extends Vue {
-  @Prop() private msg!: string;
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
